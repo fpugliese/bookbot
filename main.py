96ec3ad7a55998c07e1f9909166398cffca7bd57
Fernando Melo Pugliese <fernando@pugliese.dev>
2024-12-08 11:57:23 -0300

    Added count_characters function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def main():
-    #print_book("books/frankenstein.txt")
-    print(count_words("books/frankenstein.txt"))
+    book = "books/frankenstein.txt"
+    #print_book(book)
+    #print(count_words(book))
+    print(count_characters(book))
     
 
 def get_book(path):
-    with open ("books/frankenstein.txt") as book:
+    with open (path) as book:
         book_contents = book.read()
     return(book_contents)
 
@@ -14,8 +16,16 @@ def print_book(path):
 def count_words(path):
     count = 0
     words_number = get_book(path).split()
-    for word in words_number:
-        count +=1
-    return count
+    return len(words_number)
+
+def count_characters(path):
+    dict = {}
+    book_lowercase = get_book(path).lower()
+    for letter in book_lowercase:
+        if letter in dict:
+            dict[letter] += 1
+        else:
+            dict[letter] = 1
+    return dict
 
 main()

23671bef6331fc318cb168d4645d5ba8d446c371
Fernando Melo Pugliese <fernando@pugliese.dev>
2024-12-08 10:57:41 -0300

    First Code

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+def main():
+    #print_book("books/frankenstein.txt")
+    print(count_words("books/frankenstein.txt"))
+    
+
+def get_book(path):
+    with open ("books/frankenstein.txt") as book:
+        book_contents = book.read()
+    return(book_contents)
+
+def print_book(path):
+    print(get_book(path))
+
+def count_words(path):
+    count = 0
+    words_number = get_book(path).split()
+    for word in words_number:
+        count +=1
+    return count
+
+main()

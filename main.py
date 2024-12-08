d189e557c7515e4b86f80338f95fba749cc8fe36
Fernando Melo Pugliese <fernando@pugliese.dev>
2024-12-08 14:42:41 -0300

    Print a complete report

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     book = "books/frankenstein.txt"
-    #print_book(book)
-    #print(count_words(book))
-    print(count_characters(book))
-    
+    print(f"--- Begin report of {book} ---")
+    print(f"{count_words(book)} words found in the document")
+    list_of_characters = count_characters(book)
+    for i in list_of_characters:
+        if not i["char"].isalpha():
+            continue
+        print(f"The '{i['char']}' character was found {i['num']} times")
+    print("--- End report ---")
 
 def get_book(path):
     with open (path) as book:
@@ -26,6 +30,13 @@ def count_characters(path):
             dict[letter] += 1
         else:
             dict[letter] = 1
-    return dict
+    dict_sorted = []
+    for i in dict:
+        dict_sorted.append({"char": i, "num": dict[i]})
+    dict_sorted.sort(reverse=True, key=sort_on)
+    return dict_sorted
+
+def sort_on(dict):
+    return dict["num"]
 
 main()
